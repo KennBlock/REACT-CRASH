481bc68bfe2aff9107cb4d74749162fdaf86dd8a
Boudjehem CHEMS EDDINE <chamsnfs@gmail.com>
2024-10-25 22:01:59 +0100

    Refactor App.js and add MyFirstComponent

---
+++ src/components/MyFirstComponent.js
@@ -0,0 +1,23 @@
+import "./MyFirstComponent.css"
+export default function MyFirstComponent() {
+  const person = {
+    name: 'Chamseddine',
+    email: 'chamsnfs@gmail.com',
+    age: 22,
+  };
+  const elmStyle = {
+    backgroundColor: 'red',
+    fontSize: '24px',
+    boxShadow: '0 20px 10px black',
+  };
+  return (
+    <div>
+      <h1 style={elmStyle}>{person.email}</h1>
+      <h2 className={person.name === "mino" ? 'active' : 'inactive'}>Hello World</h2>
+      <button onClick={sayHello}>BOUNJOUR</button>
+    </div>
+  );
+}
+function sayHello() {
+  alert('SALAMOAYLAYKOMAYLAYKOMSALAAAAMMMMM');
+}
